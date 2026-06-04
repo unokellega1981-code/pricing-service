05653f7215058843d900bd96b3ea879dd606f6a4
JACG <jacarreragonzalez@gmail.com>
2026-06-04 10:29:42 +0200

    Creando dto/PriceResponse

---
+++ src/main/java/com/iopgrid/pricing_service/dto/PriceResponse.java
@@ -0,0 +1,28 @@
+package com.iopgrid.pricing_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PriceResponse {
+
+    private Long productId;
+
+    private Long brandId;
+
+    private Integer priceList;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    private BigDecimal price;
+}
