a4e0ff17fe51e54e31f21be4bc20c048fc79a0b3
JACG <jacarreragonzalez@gmail.com>
2026-06-04 09:40:30 +0200

    Creando entity/Price

---
+++ src/main/java/com/iopgrid/pricing_service/entity/Price.java
@@ -0,0 +1,39 @@
+package com.iopgrid.pricing_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "prices")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long brandId;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    private Integer priceList;
+
+    private Long productId;
+
+    private Integer priority;
+
+    private BigDecimal price;
+
+    private String curr;
+}
